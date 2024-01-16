8775aa1610281a3ce512a354142adbf6c1f3cb4b
Ivan <77786271+IvanBuzduha@users.noreply.github.com>
2024-01-16 18:37:42 +0200

    add some css

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import "./App.css";
 function Input(props) {
   const [todo, setTodo] = useState("");
   return (
-    
     <form
-    onSubmit={(e) => {e.preventDefault();
-      props.setTodos((prevTodos) => [
-        ...prevTodos,
-        { id: Date.now(), text: todo },
-      ]);
-      setTodo("");
-    }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        props.setTodos((prevTodos) => [
+          ...prevTodos,
+          { id: Date.now(), text: todo },
+        ]);
+        setTodo("");
+      }}
     >
       <input
-        type="text" className="input"
+        type="text"
+        className="input"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter text!"
       />
-      <button
-      type="submit"
-      className="button"
-      >
+      <button type="submit" className="button">
         Add
       </button>
     </form>
@@ -32,8 +31,10 @@ function Input(props) {
 function EditInput(props) {
   const [todo, setTodo] = useState(props.todo.text);
   return (
-    <><form
-    onSubmit={(e) => {e.preventDefault();
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
           props.setTodos((prevTodos) =>
             prevTodos.map((item) => {
               if (item.id === props.todo.id) {
@@ -44,18 +45,21 @@ function EditInput(props) {
           );
           setTodo("");
           props.setIsEdit(false);
-        }}>
-      <input
-        type="text"  className="input"
-        value={todo}
-        onChange={(e) => setTodo(e.target.value)}
-      />
-      <button
-        type="submit"className="button button-success"
+        }}
       >
-        Save
-      </button></form>
-      <button className="button" onClick={() => props.setIsEdit(false)}>Cancel</button>
+        <input
+          type="text"
+          className="input"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit" className="button button-success">
+          Save
+        </button>
+      </form>
+      <button className="button" onClick={() => props.setIsEdit(false)}>
+        Cancel
+      </button>
     </>
   );
 }
@@ -63,7 +67,11 @@ function EditInput(props) {
 function TodoItem(props) {
   const [isEdit, setIsEdit] = useState(false);
   return (
-    <div className={['sub-todo-item',props.todo.done && "done"].filter(Boolean).join(" ")}>
+    <div
+      className={["sub-todo-item", props.todo.done && "done"]
+        .filter(Boolean)
+        .join(" ")}
+    >
       {isEdit ? (
         <EditInput
           setTodos={props.setTodos}
@@ -73,33 +81,44 @@ function TodoItem(props) {
       ) : (
         props.todo.text
       )}
-      <button className="button" onClick={() => setIsEdit(true)}>Edit</button>
+
+      <button className="button" onClick={() => setIsEdit(true)}>
+        Edit
+      </button>
     </div>
   );
 }
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, text: "rrrrrrrrrrr", done: false },
+    { id: 1, text: "Get a haircut by Saturday", done: false },
+    { id: 2, text: "Do a manicure by Saturday", done: false },
   ]);
   const done = todos.filter((todo) => todo.done).length;
   return (
     <div className="App">
-      <hi>Todo App</hi>
+      <img
+        className="title"
+        src="./logo192.jpg"
+        height="20"
+        width="60"
+        alt="logo"
+      />
       <Input setTodos={setTodos} />
-      <p>
-        Done{done}/{todos.length}
+      <p className="donetext">
+        Done:<span className="button-danger"> {done}</span>/<span className="button-success">{todos.length}</span>
       </p>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} className="todo-item">
             <input
-              type="checkbox" className="input"
+              type="checkbox"
+              className="input"
               value={todo.done}
               onChange={(e) =>
                 setTodos((prevTodos) =>
                   prevTodos.map((item) => {
                     if (item.id === todo.id) {
-                      return { ...item, done: !item.done };
+                      return { ...item, done: !item.done  };
                     }
                     return item;
                   })
@@ -108,7 +127,8 @@ function App() {
             />
 
             <TodoItem todo={todo} setTodos={setTodos} />
-            <button className="button button-denger"
+            <button
+              className="button button-danger"
               onClick={() =>
                 setTodos((prevTodos) =>
                   prevTodos.filter((item) => item.id !== todo.id)
